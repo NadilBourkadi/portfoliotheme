a59c8fda5ab19659e723f4653af5c45925c392fd
Nadil Bourkadi <nadil.bourkadi@gmail.com>
2014-07-14 17:03:28 +0100

    added the portfolio entries and some styling fixes

--- index.php
+++ index.php
@@ -5,88 +5,141 @@
 
 
 <div class="main-content">
-    <div class="col-lg-12"><p>This is the main bit Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-    cillum doit Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.This is the main bit Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.This is the main bit Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-    proident, sunt in culpa qui officia deserunt mollit anim id est laborumnt, sunt in culpa qui officia deserunt mollit anim id est laborum.This is the main bit Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.This is the main bit Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.This is the main bit Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.This is the main bit Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.This is the main bit Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.This is the main bit Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-    proident, sunt in culpa qui officia deserunt mollit anim id est laborumnt, sunt in culpa qui officia deserunt mollit anim id est laborum.This is the main bit Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.This is the main bit Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.This is the main bit Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.This is the main bit Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.This is the main bit Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.This is the main bit Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <div class="col-lg-12">
+
+        <div class="top-hero">
+            <div class="col-lg-8 col-lg-offset-2">
+                <h1 class="main-title">Portfolio</h1>
+                <h3 class="sub-title">Some webs, lovingly crafted by ya boi.</h3>
+            </div>
+        </div>
+
+        <div class="portfolio-entry">
+            <div class="col-lg-12">
+                <h1 class="portfolio-entry-title">Lorem ipsum dolor sit amet</h1>
+                <hr>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 col-lg-offset-1 image-column">
+                    <div class="image-background">
+                        <a href="">
+                            <img src="http://lorempixel.com/g/1500/857/">
+                        </a>
+                        <div class="image-selector-container">
+                            <div class="image-selector"></div>
+                            <div class="image-selector"></div>
+                            <div class="image-selector"></div>
+                            <div class="image-selector"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 portfolio-description-container">
+                    <p class="portfolio-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="portfolio-entry temporary-odd-entry">
+            <div class="col-lg-12">
+                <h1 class="portfolio-entry-title">Also a thing, but a different one</h1>
+                <hr>
+            </div>
+
+            <div class="col-lg-6 col-lg-offset-1 portfolio-description-container">
+                <p class="portfolio-description">
+                    Single-origin coffee +1 Vice craft beer dreamcatcher, flexitarian pork belly Carles iPhone occupy bicycle rights Pitchfork.  Literally messenger bag kitsch iPhone yr, quinoa banh mi keytar gastropub Helvetica master cleanse.  Fingerstache church-key tofu, cred Bushwick tote bag put a bird on it photo booth fanny pack hella art party hoodie flexitarian yr Tonx.  Vinyl freegan Shoreditch banjo, polaroid tattooed next level photo booth.  Chia dreamcatcher kitsch pickled Williamsburg Shoreditch paleo.  Biodiesel Neutra pickled bicycle rights.  Mlkshk organic Shoreditch Etsy.
+                </p>
+            </div>
+
+            <div class="col-lg-4 image-column">
+                <div class="image-background">
+                    <a href="">
+                        <img src="http://lorempixel.com/n/1500/857/">
+                    </a>
+
+                    <div class="image-selector-container">
+                            <div class="image-selector"></div>
+                            <div class="image-selector"></div>
+                            <div class="image-selector"></div>
+                            <div class="image-selector"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </div>
+
+    <div class="col-lg-12">
+
+
+        <div class="portfolio-entry">
+            <div class="col-lg-12">
+                <h1 class="portfolio-entry-title">Lorem ipsum dolor sit amet</h1>
+                <hr>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 col-lg-offset-1 image-column">
+                    <div class="image-background">
+                        <a href="">
+                            <img src="http://lorempixel.com/g/1500/857/">
+                        </a>
+                        <div class="image-selector-container">
+                            <div class="image-selector"></div>
+                            <div class="image-selector"></div>
+                            <div class="image-selector"></div>
+                            <div class="image-selector"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 portfolio-description-container">
+                    <p class="portfolio-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="portfolio-entry temporary-odd-entry">
+            <div class="col-lg-12">
+                <h1 class="portfolio-entry-title">Also a thing, but a different one</h1>
+                <hr>
+            </div>
+
+            <div class="col-lg-6 col-lg-offset-1 portfolio-description-container">
+                <p class="portfolio-description">
+                    Single-origin coffee +1 Vice craft beer dreamcatcher, flexitarian pork belly Carles iPhone occupy bicycle rights Pitchfork.  Literally messenger bag kitsch iPhone yr, quinoa banh mi keytar gastropub Helvetica master cleanse.  Fingerstache church-key tofu, cred Bushwick tote bag put a bird on it photo booth fanny pack hella art party hoodie flexitarian yr Tonx.  Vinyl freegan Shoreditch banjo, polaroid tattooed next level photo booth.  Chia dreamcatcher kitsch pickled Williamsburg Shoreditch paleo.  Biodiesel Neutra pickled bicycle rights.  Mlkshk organic Shoreditch Etsy.
+                    Single-origin coffee +1 Vice craft beer dreamcatcher, flexitarian pork belly Carles iPhone occupy bicycle rights Pitchfork.  Literally messenger bag kitsch iPhone yr, quinoa banh mi keytar gastropub Helvetica master cleanse.  Fingerstache church-key tofu, cred Bushwick tote bag put a bird on it photo booth fanny pack hella art party hoodie flexitarian yr Tonx.  Vinyl freegan Shoreditch banjo, polaroid tattooed next level photo booth.  Chia dreamcatcher kitsch pickled Williamsburg Shoreditch paleo.  Biodiesel Neutra pickled bicycle rights.  Mlkshk organic Shoreditch Etsy.
+                    Single-origin coffee +1 Vice craft beer dreamcatcher, flexitarian pork belly Carles iPhone occupy bicycle rights Pitchfork.  Literally messenger bag kitsch iPhone yr, quinoa banh mi keytar gastropub Helvetica master cleanse.  Fingerstache church-key tofu, cred Bushwick tote bag put a bird on it photo booth fanny pack hella art party hoodie flexitarian yr Tonx.  Vinyl freegan Shoreditch banjo, polaroid tattooed next level photo booth.  Chia dreamcatcher kitsch pickled Williamsburg Shoreditch paleo.  Biodiesel Neutra pickled bicycle rights.  Mlkshk organic Shoreditch Etsy.
+                </p>
+            </div>
+
+            <div class="col-lg-4 image-column">
+                <div class="image-background">
+                    <a href="">
+                        <img src="http://lorempixel.com/n/1500/857/">
+                    </a>
+
+                    <div class="image-selector-container">
+                            <div class="image-selector"></div>
+                            <div class="image-selector"></div>
+                            <div class="image-selector"></div>
+                            <div class="image-selector"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>    
 </div>
+
 
 
 <?php get_footer(); ?>
