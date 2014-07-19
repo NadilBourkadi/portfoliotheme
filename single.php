b97e571aafb408be66ee0e748d018b7f5738dcbf
Nadil Bourkadi <nadil.bourkadi@gmail.com>
2014-07-19 14:00:40 +0100

    migrating single to single.php, changed main content div to table from table-row display

---
+++ single.php
@@ -0,0 +1,58 @@
+<div id="wrapper">
+
+<?php get_header(); ?>
+
+
+
+    <div class="main-content">
+        <div class="col-lg-12">
+        <?php if (have_posts()) : ?>
+        <?php while (have_posts()) : the_post(); ?>
+            <div class="top-hero">
+                <div class="col-lg-8 col-lg-offset-2">
+                    <h1 class="main-title"><a href="<?php the_permalink() ?>" rel="bookmark"> <?php the_title(); ?> </a></h1>
+                </div>
+            </div>
+
+            
+        
+        <?php endwhile; ?>
+        <?php else : ?>
+    <h6 class="center">Not Found</h6>
+        <p class="center">Sorry, but you are looking for something that isn't here.</p>
+        <?php endif; ?>
+
+        <div class="portfolio-entry">
+           
+        
+            
+            <div class="col-lg-4 image-column">
+                <div class="image-background">
+                    <a href="">
+                        <img src="http://lorempixel.com/g/1500/857/">
+                    </a>
+                    <div class="image-selector-container">
+                        <div class="image-selector"></div>
+                        <div class="image-selector"></div>
+                        <div class="image-selector"></div>
+                        <div class="image-selector"></div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-8 portfolio-description-container">
+                <div class="portfolio-description"><?php the_content(); ?></div>
+            </div>
+
+        
+        </div>
+
+
+
+        </div>
+    </div>
+
+
+
+<?php get_footer(); ?>
+
+</div>
