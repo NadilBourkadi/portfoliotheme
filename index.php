4759b358152950c7e718704eba6a6a9e9aac5015
Nadil Bourkadi <nadil.bourkadi@gmail.com>
2014-07-25 17:35:10 +0100

    tweaking pagination styles

--- index.php
+++ index.php
@@ -27,8 +27,8 @@
 
     </div>
     <div class="pagination col-lg-12">
-        <div class="nav-previous alignleft col-lg-6 col-sm-12"><?php next_posts_link( '<i class="fa fa-chevron-left"></i><span class="older">Older posts</span>' ); ?></div>
-        <div class="nav-next alignright col-lg-6"><?php previous_posts_link( '<span class="newer">Newer posts</span> <i class="fa fa-chevron-right"></i>' ); ?></div>
+        <div class="nav-previous alignleft col-lg-6 col-sm-12"><?php next_posts_link( '<i class="fa fa-chevron-left"></i><span class="older">Older</span>' ); ?></div>
+        <div class="nav-next alignright col-lg-6"><?php previous_posts_link( '<span class="newer">Newer</span> <i class="fa fa-chevron-right"></i>' ); ?></div>
     </div>
 </div>
 
