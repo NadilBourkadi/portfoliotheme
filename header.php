6c5c7257e87a4258f045d90cc130078872a95e4f
Nadil Bourkadi <nadil.bourkadi@gmail.com>
2014-07-25 14:30:58 +0100

    fixing header styles for smaller/medium screen sizes

--- header.php
+++ header.php
@@ -3,9 +3,10 @@
 <body <?php body_class($class); ?>>
 
 <header class="topbar col-lg-12">
-    <div class="col-lg-4">
+    <div class="col-lg-6 col-md-6 col-xs-12  col-sm-6">
         <h3>
             <em>Nadil Bourkadi</em>, Designer and Developer
+        </h3>
     </div>
 
     
@@ -31,7 +32,7 @@
             'container'       => 'div',
             'container_class' => '',
             'container_id'    => '',
-            'menu_class'      => 'col-lg-8',
+            'menu_class'      => 'col-lg-6 col-md-6 col-sm-6 col-xs-12',
             'menu_id'         => '',
             'echo'            => true,
             'fallback_cb'     => 'wp_page_menu',
@@ -39,10 +40,11 @@
             'after'           => '',
             'link_before'     => '',
             'link_after'      => '',
-            'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+            'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s <div style="clear: both"></div></ul>',
             'depth'           => 0,
             'walker'          => ''
         )); ?>
+        <div style="clear: both"></div>
         
 
 
