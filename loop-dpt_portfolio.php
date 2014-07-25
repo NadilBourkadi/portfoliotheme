f81c9ffda271f1b10f8f1f1abadbda0f028699d3
Nadil Bourkadi <nadil.bourkadi@gmail.com>
2014-07-25 19:31:32 +0100

    Replacing placeholders with featured images + adding proper permalinks for Portfolio entries

--- loop-dpt_portfolio.php
+++ loop-dpt_portfolio.php
@@ -12,29 +12,25 @@
                             <?php the_title(); ?></a></h1>
                         <hr>
                     </div>
-                <div class="row">
+                
                     
-                    <div class="col-lg-4 col-lg-offset-1 image-column">
+                    <div class="col-lg-3 col-lg-offset-1 image-column">
                         <div class="image-background">
-                            <a href="">
-                                <img src="http://lorempixel.com/g/1500/857/">
+                            <a href="<?php the_permalink(); ?>">
+                                <div class="featured-image" style="background-image: url('<?php echo $image[0]; ?>')">
+                                </div>
                             </a>
-                            <div class="image-selector-container">
-                                <div class="image-selector"></div>
-                                <div class="image-selector"></div>
-                                <div class="image-selector"></div>
-                                <div class="image-selector"></div>
-                            </div>
+                            
                         </div>
                     </div>
-                    <div class="col-lg-6 portfolio-description-container">
+                    <div class="col-lg-7 portfolio-description-container">
                         <div class="portfolio-description"><?php the_excerpt(); ?></div>
                     </div>
 
-                </div>
+                
                 </div>
                 <?php else: ?>
-            
+                <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
             
                 <div class="portfolio-entry temporary-odd-entry">
                     <div class="col-lg-12">
@@ -42,24 +38,18 @@
                         <hr>
                     </div>
 
-                    <div class="col-lg-6 col-lg-offset-1 portfolio-description-container">
+                    <div class="col-lg-7 col-lg-offset-1 portfolio-description-container">
                         <div class="portfolio-description">
                            <?php the_excerpt(); ?>
                         </div>
                     </div>
 
-                    <div class="col-lg-4 image-column">
+                    <div class="col-lg-3 image-column">
                         <div class="image-background">
-                            <a href="">
-                                <img src="http://lorempixel.com/n/1500/857/">
+                            <a href="<?php the_permalink(); ?>">
+                                <div class="featured-image" style="background-image: url('<?php echo $image[0]; ?>')">
+                                </div>
                             </a>
-
-                            <div class="image-selector-container">
-                                    <div class="image-selector"></div>
-                                    <div class="image-selector"></div>
-                                    <div class="image-selector"></div>
-                                    <div class="image-selector"></div>
-                            </div>
                         </div>
                     </div>
                 </div>
